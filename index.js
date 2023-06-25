6cbbd3294ac3c89cfe235b3e408dda89c360b53a
unknown <1286439347@qq.com>
2023-06-25 09:13:08 +0800

    测试

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ const http = require("http");
 const https = require("https");
 const static = require("koa-static");
 const app = new koa();
-const options = {
-    key: fs.readFileSync(
-        path.join(__dirname, "./certificate/lcc.key")
-    ),
-    cert: fs.readFileSync(
-        path.join(__dirname, "./certificate/lcc.pem")
-    )
-};
-const httpsServer = https.createServer(options, app.callback());
-httpsServer.listen(7004);
+const Router = require('koa-router');
+const router = new Router();
+
+
+
+// require("./mqtt/index");
+// mqtt.mqtt(httpsServer);
+
+
+
+// const options = {
+//     key: fs.readFileSync(
+//         path.join(__dirname, "./server.key")
+//     ),
+//     cert: fs.readFileSync(
+//         path.join(__dirname, "./server.pem")
+//     )
+// };
+
+// const httpsServer = https.createServer(options, app.callback());
+// httpsServer.listen(7004);
 
 const httpServer = http.Server(app.callback());
 httpServer.listen(7005);
 
 const socket = require("./socket/index");
-socket.socket(httpsServer);
+socket.socket(httpServer);
 
 // app.use(static(__dirname + "/static"));
-app.use(static(__dirname));
+app.use(static(__dirname)).use(router.routes()).use(router.allowedMethods())
 // app.listen(3000);
